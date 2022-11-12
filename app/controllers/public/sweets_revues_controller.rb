3e426675f2fe312d7b1fe1c9ea20225c60b193c6
hayashiyukino <112701375+hayashiyukino@users.noreply.github.com>
2022-11-12 21:49:56 +0000

    投稿ページ作成途中

---
+++ app/controllers/public/sweets_revues_controller.rb
@@ -0,0 +1,14 @@
+class Public::SweetsRevuesController < ApplicationController
+  def new
+    @sweets_revue = SweetsRevue.new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
