54e2316cb3e685df86a9abd518eb3e9c2683bd58
hayashiyukino <112701375+hayashiyukino@users.noreply.github.com>
2022-11-14 12:04:39 +0000

    ログイン機能不具合解消

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -2,40 +2,16 @@ class EndUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-        # :authentication_keys => [:nickname]  #認証のキーをメールから名前へ
-         
+         :recoverable, :rememberable, :validatable,
+         :authentication_keys => [:nickname]  #ログイン認証のキーをニックネームへ
+
   has_many :sweets_revues
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   # has_many :
   # has_many :
-  
-  
-  # 画像投稿出来るようにする     
+
+  # 画像投稿出来るようにする
   has_one_attached :image
-         
-         
-  # validates_uniqueness_of :nickname
-  # validates_presence_of :nickname
-  
-  # ##nicknameを利用してログイン
-  # #find_first_by_auth_conditions=認証時にユーザーを特定する為に通るメソッド
-  # def self.find_first_by_auth_conditions(warden_conditions)
-  #   conditions = warden_conditions.dup
-  #   if login = conditions.delete(:login)
-  #     where(conditions).where(["nickname = :value", { :value => nickname}]).first
-  #   else
-  #     where(conditions).first
-  #   end
-  # end
-  
-  # # 登録時にemailを不要にする
-  # def email_required?
-  #   false
-  # end
-  
-  # def email_changed?
-  #   false
-  # end
+
 end
