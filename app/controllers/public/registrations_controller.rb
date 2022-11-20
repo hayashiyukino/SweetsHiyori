ee5cd2cc865f946075f43e45330bccb7240b880f
hayashiyukino <112701375+hayashiyukino@users.noreply.github.com>
2022-11-20 12:39:08 +0000

    [update]mypage評価といいね表示の部分テンプレート

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -61,7 +61,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # end
 
   def after_sign_up_path_for(resource)
-    mypage_path(resource)
+    mypage_path
   end
 
   def after_sign_out_path_for(resource)
