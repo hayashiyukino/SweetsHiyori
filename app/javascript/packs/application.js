cc91606dc988965551e723db35e529ae94b29d3a
hayashiyukino <112701375+hayashiyukino@users.noreply.github.com>
2022-11-22 18:22:04 +0000

    [add]ユーザー編集ページ作成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,7 @@ import "bootstrap";
 import "../stylesheets/application"
 import '@fortawesome/fontawesome-free/js/all'
 import '../stylesheets/public.css'
+import '../stylesheets/admin.css'
 
 import Raty from "raty.js"
 // windowに表示されているratyにfunction(引数１,引数２)の情報を渡す
