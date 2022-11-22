ac9e642d8dc59cc999b95333b261cedf559f5a57
hayashiyukino <112701375+hayashiyukino@users.noreply.github.com>
2022-11-22 09:02:24 +0000

    コンフリクト解消

---
+++ app/controllers/admin/end_users_controller.rb
@@ -0,0 +1,35 @@
+class Admin::EndUsersController < ApplicationController
+  before_action :authenticate_admin!
+  
+  def index
+    @end_users = EndUser.all.page(params[:page]).per(6)
+  end
+
+  def show
+    @end_user = EndUser.find(params[:id])
+    @sweets_revues = @end_user.sweets_revues.all.page(params[:page]).per(5).order(created_at: :desc)
+  end
+
+  def edit
+    @end_user = EndUser.find(params[:id])
+  end
+
+  def update
+    @end_user = EndUser.find(params[:id])
+   if @end_user.update(end_user_params)
+      redirect_to admin_end_user_path(@end_user.id)
+   else
+      render :edit
+   end
+  end
+  
+  private
+
+  def end_user_params
+    params.require(:end_user).permit(
+      :name, :nickname, :introduction, :user_status, :profile_image
+    )
+  end
+  
+  
+end
