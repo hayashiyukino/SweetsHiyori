f1fef2593c91da8630b5b18f2bed5f102e586c79
hayashiyukino <112701375+hayashiyukino@users.noreply.github.com>
2022-11-22 07:53:38 +0000

    [add]adminユーザー詳細ページ作成

--- app/controllers/admin/end_users_controller.rb
+++ app/controllers/admin/end_users_controller.rb
@@ -2,8 +2,10 @@ class Admin::EndUsersController < ApplicationController
   def index
     @end_users = EndUser.all.page(params[:page]).per(6)
   end
-  
+
   def show
+    @end_user = EndUser.find(params[:id])
+    @sweets_revues = @end_user.sweets_revues.all.page(params[:page]).per(5).order(created_at: :desc)
   end
 
   def edit
