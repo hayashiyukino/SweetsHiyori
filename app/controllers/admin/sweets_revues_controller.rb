facf7a89dd9d7e3adba39d9230044d5de88f444e
hayashiyukino <112701375+hayashiyukino@users.noreply.github.com>
2022-11-22 18:55:10 +0000

    [add]投稿詳細ページ

--- app/controllers/admin/sweets_revues_controller.rb
+++ app/controllers/admin/sweets_revues_controller.rb
@@ -2,5 +2,9 @@ class Admin::SweetsRevuesController < ApplicationController
   before_action :authenticate_admin!
   
   def show
+    @sweets_revue = SweetsRevue.find(params[:id])
+    @post_comment = PostComment.new
+    @end_user = @sweets_revue.end_user
   end
+  
 end
