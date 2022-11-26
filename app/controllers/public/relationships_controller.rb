1c9f06323fa4cffa9e67a550d6c9f55c5aeb885c
hayashiyukino <112701375+hayashiyukino@users.noreply.github.com>
2022-11-26 19:50:22 +0000

    フォロー機能非同期途中

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -5,12 +5,10 @@ class Public::RelationshipsController < ApplicationController
   # follow(params[:end_user_id])で、models/end_user.rbで定義したメソッドを呼び出す
   def create
     current_end_user.follow(params[:end_user_id])
-    redirect_to request.referer
   end
   # フォロー外すとき
   def destroy
     current_end_user.unfollow(params[:end_user_id])
-    redirect_to request.referer
   end
   # フォロー一覧
   def followings
