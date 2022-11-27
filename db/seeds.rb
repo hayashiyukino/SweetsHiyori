e233c0138fe3445df51bfe348587cf996c6c75d9
hayashiyukino <112701375+hayashiyukino@users.noreply.github.com>
2022-11-27 09:25:19 +0000

    [add]seedsファイル

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,34 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Admin.create!(
-   email: 'sweets@hiyori',
-   password: '123456'
-)
 
-EndUser.create!(
-   email: 'test@1',
-   password: '789789',
-   name: "testname",
-   nickname: "tes"
-)
 
-#ジャンル名
-Genre.create!(name:"洋菓子")
-Genre.create!(name:"焼き菓子")
-Genre.create!(name:"半生菓子")
-Genre.create!(name:"和菓子")
+# rails db:seed -e production
 
-#タグ名
-Tag.create!(name:"洋菓子店")
-Tag.create!(name:"デパート")
-Tag.create!(name:"ホテル")
-Tag.create!(name:"コンビニ")
-Tag.create!(name:"かわいい")
-Tag.create!(name:"おすすめ")
-Tag.create!(name:"テイクアウト")
-Tag.create!(name:"イートイン")
-Tag.create!(name:"カフェ")
-Tag.create!(name:"お土産")
-Tag.create!(name:"ご褒美")
-Tag.create!(name:"安い")
+admins = [
+   {email: 'sweets@hiyori', password: '123456'},
+]
+
+admins.each do |admin|
+   admin_data = Admin.find_by(email: admin[:email])
+   if admin_data.nil?
+      Admin.create(email: admin[:email], password: admin[:password])
+   end
+end
+
+
+15.times do |num|
+   user_data = EndUser.find_by(email: "test#{num + 1}@test")
+   if user_data.nil?
+      EndUser.create(
+         email: "test#{num + 1}@test",
+         password: "test#{num + 1}0000",
+         name: "テスト名前#{num + 1}",
+          nickname: "ニックネーム#{num + 1}"
+      )
+   end
+end
+
+
+genres = ['生菓子', '焼き菓子', '半生菓子', '和菓子', 'その他']
+genres.each do |genre|
+   Genre.find_or_create_by(name: genre)
+end
+
+
+
+tags = [
+   '洋菓子店', 'デパート', 'ホテル', 'コンビニ', 'かわいい', 'おすすめ',
+   'テイクアウト', 'イートイン',
+   'カフェ', 'お土産', 'お手頃価格', 'パフェ', '映え', 'ボリューム', 'ご当地', 'おしゃれ',
+   'その他'
+]
+tags.each do |tag|
+   Tag.find_or_create_by(name: tag)
+end
