8a86cf3cd0a192bc9e68586d5016715e9288fa6d
hayashiyukino <112701375+hayashiyukino@users.noreply.github.com>
2022-12-16 19:44:16 +0000

    [update]コメント削除機能非同期処理

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -2,16 +2,18 @@ class Public::PostCommentsController < ApplicationController
   before_action :authenticate_end_user!
 
   def create
-    sweets_revue = SweetsRevue.find(params[:sweets_revue_id])
-    comment = current_end_user.post_comments.new(post_comment_params)
-    comment.sweets_revue_id = sweets_revue.id
-    comment.save
-    redirect_to sweets_revue_path(sweets_revue)
+    @sweets_revue = SweetsRevue.find(params[:sweets_revue_id])
+    @comment = current_end_user.post_comments.new(post_comment_params)
+    @comment.sweets_revue_id = @sweets_revue.id
+    @comment.save
+      redirect_to sweets_revue_path(@sweets_revue)
+    # render :post_comments
   end
 
   def destroy
     PostComment.find(params[:id]).destroy
-    redirect_to sweets_revue_path(params[:sweets_revue_id])
+    @sweets_revue = SweetsRevue.find(params[:sweets_revue_id])
+    render :post_comments
   end
 
   private
